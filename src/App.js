570a2851fe7faa1e31e01a36aa0ebfe4f501f133
Boissé Olivier <oboisse.prestataire@oui.sncf>
2019-02-22 17:54:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import {MuiThemeProvider} from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import {createMuiTheme} from "@material-ui/core";
+import blue from '@material-ui/core/colors/blue';
+import orange from '@material-ui/core/colors/orange';
+import red from '@material-ui/core/colors/red';
+import TabContainer from "./TabContainer";
+import UseStateExample from "./examples/UseStateExample";
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+    secondary: orange,
+    error: red
   }
+});
+
+function App({classes}) {
+  const [value, setValue] = useState(0);
+
+  function handleChange(event, value) {
+    setValue(value);
+  }
+
+  return (
+    <MuiThemeProvider theme={theme}>
+      <div>
+        <AppBar position="static">
+          <Tabs value={value} onChange={handleChange}>
+            <Tab label="USE STATE" />
+            <Tab label="USE EFFECT" />
+          </Tabs>
+        </AppBar>
+        {value === 0 && <TabContainer><UseStateExample/></TabContainer>}
+        {value === 1 && <TabContainer>UseEffect</TabContainer>}
+      </div>
+    </MuiThemeProvider>
+  );
 }
 
 export default App;
