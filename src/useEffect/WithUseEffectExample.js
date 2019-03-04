c3b4f937d2aa1bb98fa69b334d01ce22eab464c5
oliv37 <olivier.boisse@laposte.net>
2019-03-04 19:32:11 +0100

    fix UseEffect example

--- src/useEffect/WithUseEffectExample.js
+++ src/useEffect/WithUseEffectExample.js
@@ -3,9 +3,15 @@ import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 
 function WithUseEffectExample() {
+    const [name, setName] = useState("john");
+
+    function handleChangeName(e) {
+        setName(e.target.value);
+    }
+
     return (
         <>
-            <TextField margin="normal"/>
+            <TextField margin="normal" value={name} onChange={handleChangeName}/>
             <Typography component="p" variant="body1" style={{margin: "15px 0"}}>
                 {`... px`}
             </Typography>
