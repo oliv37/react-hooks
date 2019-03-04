1441850b399a6cabcc4ee3e530a313b447f44aff
Boissé Olivier <oboisse.prestataire@oui.sncf>
2019-03-04 09:24:40 +0100

    step1

--- src/useEffect/WithUseEffectExample.js
+++ src/useEffect/WithUseEffectExample.js
@@ -3,13 +3,11 @@ import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 
 function WithUseEffectExample() {
-    const width = window.innerWidth;
-    
     return (
         <>
             <TextField margin="normal"/>
             <Typography component="p" variant="body1" style={{margin: "15px 0"}}>
-                {`${width} px`}
+                {`... px`}
             </Typography>
         </>
     );
--- src/useState/WithUseStateExample.js
+++ src/useState/WithUseStateExample.js
@@ -3,14 +3,12 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 
 function WithUseStateExample() {
-    const counter = 0;
-    
     return (
         <>
             <TextField margin="normal"/>
 
             <Button variant="contained" color="primary">
-                {`Value = ${counter}`}
+                {`Value = 0`}
             </Button>
         </>
     );
